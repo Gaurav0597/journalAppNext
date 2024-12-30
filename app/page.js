915035d2fabc54e827133b4d7109bf75747e754b
Gaurav <gaurav@indiadatahub.com>
2024-12-30 18:46:44 +0530

    shadcn installed and component is added succesfully

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
+  const hello = 1
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    Gaurav
+    <div >
+         Journal App 
+         <br></br>
+         <Button variant="journal">Button</Button>
     </div>
   );
 }
+      
